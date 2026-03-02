e557ea3a0eac17bfe536b3c0c7023537c8d94a71
Nagy Zétény <nagyzeteny6@gmail.com>
2026-03-02 10:43:48 +0100

    chore: init

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f ".env" ]; then
+    echo ".env file already exists. Skipping..."
+else
+    echo ".env file does not exist. Copying from .env.example..."
+    cp .env.example .env
+fi
+
+if docker volume inspect shared_pnpm >/dev/null 2>&1; then
+    echo "Docker volume 'shared_pnpm' already exists. Skipping..."
+else
+    echo "Docker volume 'shared_pnpm' does not exist. Creating..."
+    docker volume create shared_pnpm
+fi
+
+docker compose up -d
